f057714c5392515e6f865232f2c9924155d67ebf
berzins1 <mberzins1@gmail.com>
2018-10-23 18:58:03 -0500

    added stuff

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            changed on the feature branch
+            still trying to get it to work
           </p>
           <a
             className="App-link"
