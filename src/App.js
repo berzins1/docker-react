42338dc1959aa130031f9d09383b59aa21afd4f9
berzins1 <mberzins1@gmail.com>
2018-10-23 22:03:02 -0500

    feature

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            still trying to get it to work
+            feature pull
           </p>
           <a
             className="App-link"
